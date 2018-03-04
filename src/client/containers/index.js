73fcce678ab104e26ae7ca5d965ba783fa6ccde8
James Dempsey <jd15497@it025971.users.bris.ac.uk>
2018-03-04 10:16:03 +0000

    Register Page

--- src/client/containers/index.js
+++ src/client/containers/index.js
@@ -1,9 +1,11 @@
 import { ChessGame } from './ChessGame'
 import { InstantMessenger } from './InstantMessenger'
 import { LoginForm } from './LoginForm'
+import { RegisterForm } from './RegisterForm'
 
 export {
     ChessGame,
     InstantMessenger,
     LoginForm,
+    RegisterForm,
 }
